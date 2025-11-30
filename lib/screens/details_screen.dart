194db652950bb5745676e4855539a75e0925909f
Edison Zheng <sketchyazx@gmail.com>
2025-11-30 15:25:20 -0500

    Changes details screen top image into an image carousel to display multiple images

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,25 +3,86 @@
 
 import 'package:flutter/material.dart';
 
-class DetailsScreen extends StatelessWidget {
+class DetailsScreen extends StatefulWidget {
   final Map<String, dynamic> property;
 
   const DetailsScreen({super.key, required this.property});
 
   @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  int currentIndex = 0;
+
+  late final List<String> images;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Placeholder images for carousel
+    images = [
+      widget.property["image"],
+      widget.property["image"],
+      widget.property["image"],
+      widget.property["image"],
+      widget.property["image"],
+    ];
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(property["title"])),
+      appBar: AppBar(title: Text(widget.property["title"])),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Top Image
-            Image.network(
-              property["image"],
-              width: double.infinity,
+            // Image Carousel
+            SizedBox(
               height: 260,
-              fit: BoxFit.cover,
+              child: Stack(
+                children: [
+                  PageView.builder(
+                    itemCount: images.length,
+                    onPageChanged: (index) {
+                      setState(() => currentIndex = index);
+                    },
+                    itemBuilder: (context, index) {
+                      return Image.network(
+                        images[index],
+                        width: double.infinity,
+                        height: 260,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
+
+                  // Carousel image indicators
+                  Positioned(
+                    bottom: 10,
+                    left: 0,
+                    right: 0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(images.length, (index) {
+                        return Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 4),
+                          width: currentIndex == index ? 12 : 8,
+                          height: 8,
+                          decoration: BoxDecoration(
+                            color: currentIndex == index
+                                ? Colors.white
+                                : Colors.white54,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
+                ],
+              ),
             ),
 
             Padding(
@@ -31,7 +92,7 @@ class DetailsScreen extends StatelessWidget {
                 children: [
                   // Title
                   Text(
-                    property["title"],
+                    widget.property["title"],
                     style: const TextStyle(
                       fontSize: 22,
                       fontWeight: FontWeight.bold,
@@ -42,7 +103,7 @@ class DetailsScreen extends StatelessWidget {
 
                   // Location
                   Text(
-                    property["location"],
+                    widget.property["location"],
                     style: TextStyle(
                       color: Colors.grey.shade700,
                       fontSize: 16,
@@ -56,7 +117,7 @@ class DetailsScreen extends StatelessWidget {
                     children: [
                       const Icon(Icons.attach_money, color: Colors.green),
                       Text(
-                        "\$${property["value"]}",
+                        "\$${widget.property["value"]}",
                         style: const TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
@@ -72,8 +133,14 @@ class DetailsScreen extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      _infoBox("Bedrooms", property["bedrooms"].toString()),
-                      _infoBox("Bathrooms", property["bathrooms"].toString()),
+                      _infoBox(
+                        "Bedrooms",
+                        widget.property["bedrooms"].toString(),
+                      ),
+                      _infoBox(
+                        "Bathrooms",
+                        widget.property["bathrooms"].toString(),
+                      ),
                     ],
                   ),
                 ],
